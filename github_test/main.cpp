91c2a0495a17918a8fe9617f6678850ef5c92e7c
lEe_NiAnsAn <123313005+lEe-NiAnsAn@users.noreply.github.com>
2024-08-22 22:56:32 +0800

    VS中Git无法推送代码时的一个可能的解决方法

--- github_test/main.cpp
+++ github_test/main.cpp
@@ -4,6 +4,8 @@ using namespace std;
 int main(void)
 {
 	cout << "Hello World!\n";
+	cout << "如遇VS无法推送代码，可尝试将系统盘下用户文件夹内的.gitconfig文件编辑补充字段\n"
+		<< "[safe]\n" << "\t directory=*" << endl;
 
 	system("pause");
 
