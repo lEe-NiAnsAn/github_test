68a932b21f9375238d14de3452e1c3cb0656f509
lEe_NiAnsAn <123313005+lEe-NiAnsAn@users.noreply.github.com>
2024-08-22 22:59:11 +0800

    美观

--- github_test/main.cpp
+++ github_test/main.cpp
@@ -1,11 +1,11 @@
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
 
 int main(void)
 {
 	cout << "Hello World!\n";
 	cout << "如遇VS无法推送代码，可尝试将系统盘下用户文件夹内的.gitconfig文件编辑补充字段\n"
-		<< "[safe]\n" << "\t directory=*" << endl;
+		<< "[safe]\n" << "    directory=*" << endl;
 
 	system("pause");
 
